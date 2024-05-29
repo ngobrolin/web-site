93e47ef1631456161b9ebea8a6c676859d917b8b
Riza Fahmi <rizafahmi@gmail.com>
2024-05-29 17:48:33 +0700

    Add test.watch

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Ngobrolin.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:mix_test_watch, "~> 1.2", only: [:test, :dev]}
     ]
   end
 
