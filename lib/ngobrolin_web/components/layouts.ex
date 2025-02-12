5373928bced038faa0afe2a5acaf62d959916e55
Riza Fahmi <rizafahmi@gmail.com>
2025-02-12 18:14:35 +0700

    Fresh start. Init

---
+++ lib/ngobrolin_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule NgobrolinWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use NgobrolinWeb, :controller` and
+  `use NgobrolinWeb, :live_view`.
+  """
+  use NgobrolinWeb, :html
+
+  embed_templates "layouts/*"
+end
