c04e9b7f50756c4e0e68b2f718ff73d58bf382a0
Riza Fahmi <rizafahmi@gmail.com>
2025-03-24 03:45:46 +0700

    Add test_interactive

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Ngobrolin.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:tzdata, "~> 1.1"}
+      {:tzdata, "~> 1.1"},
+      {:mix_test_interactive, "~> 4.3", only: :dev, runtime: false}
     ]
   end
 
