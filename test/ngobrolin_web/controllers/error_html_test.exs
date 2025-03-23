69dcaa99f7e6e8560b2cff31e73a9e55b440c9fe
Riza Fahmi <rizafahmi@gmail.com>
2025-03-23 21:56:38 +0700

    Update error HTML tests to check for localized error messages

--- test/ngobrolin_web/controllers/error_html_test.exs
+++ test/ngobrolin_web/controllers/error_html_test.exs
@@ -5,10 +5,13 @@ defmodule NgobrolinWeb.ErrorHTMLTest do
   import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(NgobrolinWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    html = render_to_string(NgobrolinWeb.ErrorHTML, "404", "html", [])
+    assert html =~ "HALAMAN TIDAK DITEMUKAN"
+    assert html =~ "404"
   end
 
   test "renders 500.html" do
-    assert render_to_string(NgobrolinWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    html = render_to_string(NgobrolinWeb.ErrorHTML, "500", "html", [])
+    assert html =~ "SERVER ERROR"
   end
 end
