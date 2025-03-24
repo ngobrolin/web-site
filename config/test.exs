22e677eef9db001241081d8760ef027eb1204bc2
Riza Fahmi <rizafahmi@gmail.com>
2025-03-25 05:14:23 +0700

    Update test configuration with PostgreSQL credentials

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ngobrolin, Ngobrolin.Repo,
-  username: "riza",
-  password: "",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost",
   database: "ngobrolin_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
